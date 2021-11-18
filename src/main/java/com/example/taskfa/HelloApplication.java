dfd8338e9d43291a5b1da79920067c82c774de1c
herkane <herkane2014@gmail.com>
2021-11-18 23:34:39 +0100

    first commit

---
+++ src/main/java/com/example/taskfa/HelloApplication.java
@@ -0,0 +1,31 @@
+package com.example.taskfa;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+        scene.getStylesheets().add(getClass().getResource("styles/style.css").toExternalForm());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.setFullScreenExitHint("");
+        stage.setFullScreen(true);
+        stage.setResizable(false);
+
+        stage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.taskfa {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+
+    opens com.example.taskfa to javafx.fxml;
+    exports com.example.taskfa;
+    exports com.example.taskfa.controllers;
+    opens com.example.taskfa.controllers to javafx.fxml;
+}
