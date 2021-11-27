c3473b671e0711d4948d27a547322beea5beb8f0
herkane <herkane2014@gmail.com>
2021-11-27 21:06:32 +0100

    Finished SignUp UI

--- src/main/java/com/example/taskfa/controllers/SignUpController.java
+++ src/main/java/com/example/taskfa/controllers/SignUpController.java
@@ -11,4 +11,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class SignUpController {
+    @FXML
+    private Button reduceBtn,closeBtn;
+
+    public void closeWin(){
+        Stage stage = (Stage) closeBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    public void reduceWin(){
+        Stage stage = (Stage) reduceBtn.getScene().getWindow();
+        stage.setIconified(true);
+    }
 }
