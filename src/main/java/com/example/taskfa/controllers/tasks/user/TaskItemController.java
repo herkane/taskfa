8309dede1ee2fc5f8472c1aaf8bab6de06543c15
Aissam-gif <aissamboussoufiane999@gmail.com>
2022-01-27 02:07:46 +0100

    update some UI for task item controller for user

--- src/main/java/com/example/taskfa/controllers/tasks/user/TaskItemController.java
+++ src/main/java/com/example/taskfa/controllers/tasks/user/TaskItemController.java
@@ -1,16 +1,21 @@
 package com.example.taskfa.controllers.tasks.user;
 
 import com.example.taskfa.controllers.tasks.TaskStatus;
+import javafx.beans.property.DoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -62,41 +67,70 @@ public class TaskItemController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         taskItemController = this;
+
         // TODO
         btnInfo.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 if (tasksModel.getStatus() == TaskStatus.NOTSTARTED) {
                     tasksModel.setStatus(TaskStatus.PENDING);
+                    iconSelect.setImage(new Image(getClass().getResourceAsStream("/media/pending_icon_task.png")));
+                    btnInfo.setText("Pending");
                     tasksModel.setCompleted(false);
                 } else if (tasksModel.getStatus() == TaskStatus.PENDING) {
                     tasksModel.setStatus(TaskStatus.DONE);
+                    iconSelect.setImage(new Image(getClass().getResourceAsStream("/media/icons8_checked_filled_24px_1.png")));
+                    btnInfo.setText("Done");
                     tasksModel.setCompleted(true);
                 } else {
                     tasksModel.setStatus(TaskStatus.NOTSTARTED);
+                    iconSelect.setImage(new Image(getClass().getResourceAsStream("/media/icons8_checked_filled_24px.png")));
+                    btnInfo.setText("Not Started");
                     tasksModel.setCompleted(false);
                 }
 
                 taskItemControllerConsumer.accept(taskItemController);
             }
         });
+        DropShadow shadow = new DropShadow();
+        btnInfo.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        lblTaskName.getScene().setCursor(Cursor.HAND);
+                        if (tasksModel.getStatus() == TaskStatus.NOTSTARTED) {
+                            shadow.setColor(Color.ORANGE);
+                            btnInfo.setEffect(shadow);
+                        } else if (tasksModel.getStatus() == TaskStatus.PENDING) {
+                            shadow.setColor(Color.GREEN);
+                            btnInfo.setEffect(shadow);
+                        } else {
+                            shadow.setColor(Color.RED);
+                            btnInfo.setEffect(shadow);
+                        }
+                    }
+                });
+        btnInfo.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        lblTaskName.getScene().setCursor(Cursor.DEFAULT);
+                        btnInfo.setEffect(null);
+                    }
+                });
     }
 
     public void setTask(TasksModel model, int position) {
         this.position = position;
         this.tasksModel = model;
-        ContextMenu menu = new ContextMenu();
-        System.out.println(model.toString() + " i'm heeere");
         lblTaskName.setText(model.getTitle());
-        if (model.getCompleted()) {
-            btnInfo.setText("Complete");
-            iconSelect.setImage(new Image(getClass().getResourceAsStream("/media/icons8_checked_filled_24px_1.png")));
-            menu.getItems().add(new MenuItem("Set Task InComplete"));
-        } else {
-            btnInfo.setText("InComplete");
+        if (model.getStatus() == TaskStatus.NOTSTARTED) {
+            btnInfo.setText("Not Started");
             iconSelect.setImage(new Image(getClass().getResourceAsStream("/media/icons8_checked_filled_24px.png")));
-            menu.getItems().add(new MenuItem("Set Task Complete"));
+        } else if (model.getStatus() == TaskStatus.PENDING) {
+            btnInfo.setText("Pending");
+            iconSelect.setImage(new Image(getClass().getResourceAsStream("/media/pending_icon_task.png")));
+        } else {
+            btnInfo.setText("Done");
+            iconSelect.setImage(new Image(getClass().getResourceAsStream("/media/icons8_checked_filled_24px_1.png")));
         }
-        lblTaskName.setContextMenu(menu);
     }
 }
