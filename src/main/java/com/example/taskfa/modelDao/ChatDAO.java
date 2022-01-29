886c5932900963f5ad1d905a18706485cb9c2824
laouissi <a.laouissi3@gmail.com>
2022-01-29 12:48:54 +0100

    insert messages in data base

--- src/main/java/com/example/taskfa/modelDao/ChatDAO.java
+++ src/main/java/com/example/taskfa/modelDao/ChatDAO.java
@@ -9,10 +9,9 @@ import javafx.collections.ObservableList;
 import javafx.scene.image.Image;
 
 import java.io.InputStream;
-import java.sql.Blob;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ChatDAO {
@@ -20,7 +19,7 @@ public class ChatDAO {
     public static ObservableList<Message> searchMessages(int projectId) throws SQLException, ClassNotFoundException {
         String selectStm = "SELECT message_content,time_sent,lastName,image,iduser FROM message " +
                 "INNER JOIN user ON user_iduser = iduser WHERE project_projectid = "+projectId+" " +
-                "ORDER BY time_sent DESC;";
+                "ORDER BY time_sent ASC;";
         try {
             ResultSet rsProjects = DBConfig.dbExecuteQuery(selectStm);
             ObservableList<Message> projectlist = getMessageList(rsProjects);
@@ -35,7 +34,7 @@ public class ChatDAO {
     public static ObservableList<Message> searchMessagesLimit(int projectId) throws SQLException, ClassNotFoundException {
         String selectStm = "SELECT message_content,time_sent,lastName,image,iduser FROM message " +
                 "INNER JOIN user ON user_iduser = iduser WHERE project_projectid = "+projectId+" " +
-                "ORDER BY time_sent DESC LIMIT 10;";
+                "ORDER BY time_sent ASC LIMIT 10;";
         try {
             ResultSet rsProjects = DBConfig.dbExecuteQuery(selectStm);
             ObservableList<Message> projectlist = getMessageList(rsProjects);
@@ -49,7 +48,8 @@ public class ChatDAO {
 
     private static ObservableList<Message> getMessageList(ResultSet rs) throws SQLException {
         ObservableList<Message> messagesList = FXCollections.observableArrayList();
-        DateFormat df = new SimpleDateFormat("hh:mm");
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+        DateFormat dfDisplay = new SimpleDateFormat("hh-mm");
         while (rs.next()) {
             Message message = new Message();
             User user = new User();
@@ -61,13 +61,36 @@ public class ChatDAO {
             user.setImage(image);
             message.setSender(user);
             message.setMessage(rs.getString("message_content"));
-            message.setDate_sent(df.format(rs.getTimestamp("time_sent")));
+            message.setDate_sent(dfDisplay.format(rs.getTimestamp("time_sent")));
             System.out.println(message);
             messagesList.add(message);
         }
 
         return messagesList;
     }
+
+    public static void InsertMessage( String message_content, String time_sent, int user_iduser, int project_projectid) throws ClassNotFoundException {
+
+        String insertStmtprepared = "INSERT INTO message " +
+                "( message_content, time_sent, user_iduser,  project_projectid) " +
+                "VALUES " +
+                "(?,?,?,?);";
+
+        try {
+            DBConfig.dbConnect();
+            Connection conn = DBConfig.getConn();
+            PreparedStatement ps = conn.prepareStatement(insertStmtprepared);
+            ps.setString(1, message_content);
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+            Timestamp t = new Timestamp(formatter.parse(time_sent).getTime());
+
+            ps.setTimestamp(2, t);
+            ps.setInt(3, user_iduser);
+            ps.setInt(4,project_projectid);
+            ps.executeUpdate();
+        } catch (SQLException | ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
-
-
